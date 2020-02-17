1f6c69ea7cf4f55f57d02df92b4bbec31afec2fc
Pravin Vegare <Pravinvegare123@gmail.com>
2020-02-17 17:40:59 +0530

    Error handling for recipe update route handler method

--- recipe_service/controller/commands/addRecipe.js
+++ recipe_service/controller/commands/addRecipe.js
@@ -1,7 +1,22 @@
 const { addRecipe } = require("../../recipe/index");
 const Promise = require("promise");
+
+// This function responsible for inserting  recipe
 exports.addRecipeController = async(req, res) => {
+
     const { recipeInfo } = req.body;
-    const recipe = await addRecipe(recipeInfo)
-    res.status(200).json(recipe);
+    addRecipe(recipeInfo)
+        .then(data => {
+            return res.status(201).json({
+                "status": 201,
+                "data": data
+            });
+        })
+        .catch(err => {
+            return res.status(500).json({
+                "status": 500,
+                "Error": err.errmsg
+            });
+        })
+
 }
